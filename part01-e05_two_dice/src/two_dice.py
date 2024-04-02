508c9adcc1e5fd508f9bb9d5ef041f87256cdb43
Polaski <101113655+2Tough@users.noreply.github.com>
2024-04-02 13:49:19 +0300

    Completed 2 more exercises

---
+++ part01-e05_two_dice/src/two_dice.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def main():
+    pass
+
+
+    for i in [1,2,3,4,5,6]:
+        for j in [1,2,3,4,5,6]:
+            if (i+j == 5):
+                print(f"({i},{j})")
+
+
+if __name__ == "__main__":
+    main()
+
